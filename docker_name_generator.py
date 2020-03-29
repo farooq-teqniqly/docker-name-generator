d087c3b93d7fa8a3821e0fba7d203bac4f1f0e20
farooq-teqniqly <49829654+farooq-teqniqly@users.noreply.github.com>
2020-03-28 22:01:12 -1000

    Docker name generator
    Co-authored-by: Farooq Mahmud <f>

---
+++ docker_name_generator.py
@@ -0,0 +1,58 @@
+import io
+import random
+from itertools import islice
+
+
+def __read_file(path, target_list):
+    """
+    Reads a file and appends each line to a list.
+
+    Args:
+        path: The path to the file to read.
+        target_list: The list to append each line to.
+
+    Returns:
+        None
+    """
+    with io.open(path, "r", encoding="utf8") as f:
+        try:
+            for line in f:
+                target_list.append(line.replace(" ", "").strip())
+        except UnicodeDecodeError:
+            pass
+
+
+def load_files(names_file, nouns_file):
+    """
+    Loads a names and nouns file.
+
+    Args:
+        names_file: The path to the names file.
+        nouns_file: The path to the nouns file.
+
+    Returns:
+        A two element tuple. The first element is a list containing the names. The second element is a list
+        containing the nouns. The lengths of each list is limited to 1000 elements.
+    """
+    max_results_to_return = 1000
+    names = []
+    nouns = []
+
+    __read_file(names_file, names)
+    __read_file(nouns_file, nouns)
+
+    return list(set(islice(names, max_results_to_return))), list(set(islice(nouns, max_results_to_return)))
+
+
+def generate_name(names_nouns_tuple):
+    """
+    Generates a Docker style name in the format 'name_noun'.
+
+    Args:
+        names_nouns_tuple: An two element tuple. The first element is a list containing the names. The second element is
+        a list containing the nouns.
+    Returns:
+        A name in the format 'name_noun'.
+    """
+    names, nouns = names_nouns_tuple
+    return f"{random.choice(names)}_{random.choice(nouns)}".lower()
