85b0bef09b420daf78b208053f2e43167ee67fe1
farooq-teqniqly <49829654+farooq-teqniqly@users.noreply.github.com>
2020-03-28 22:07:57 -1000

    Name generator refactor
    Co-authored-by: Farooq Mahmud <f>

--- docker_name_generator.py
+++ docker_name_generator.py
@@ -3,21 +3,22 @@ import random
 from itertools import islice
 
 
-def __read_file(path, target_list):
+def __read_file(path, target_set):
     """
     Reads a file and appends each line to a list.
 
     Args:
         path: The path to the file to read.
-        target_list: The list to append each line to.
+        target_set: The set to append each line to.
 
     Returns:
         None
     """
+
     with io.open(path, "r", encoding="utf8") as f:
         try:
             for line in f:
-                target_list.append(line.replace(" ", "").strip())
+                target_set.add(line.replace(" ", "").strip())
         except UnicodeDecodeError:
             pass
 
@@ -35,13 +36,13 @@ def load_files(names_file, nouns_file):
         containing the nouns. The lengths of each list is limited to 1000 elements.
     """
     max_results_to_return = 1000
-    names = []
-    nouns = []
+    names = set()
+    nouns = set()
 
     __read_file(names_file, names)
     __read_file(nouns_file, nouns)
 
-    return list(set(islice(names, max_results_to_return))), list(set(islice(nouns, max_results_to_return)))
+    return list(islice(names, max_results_to_return)), list(islice(nouns, max_results_to_return))
 
 
 def generate_name(names_nouns_tuple):
